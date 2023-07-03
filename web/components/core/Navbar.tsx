cab69cdeee9434f126b99cb25221f1507b4df4da
Vivian Leung <vivz753@gmail.com>
2023-07-03 16:37:31 -0700

    link electricpotential to homepage on navbar

--- web/components/core/Navbar.tsx
+++ web/components/core/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className="w-screen bg-white gap-4 py-[25px] px-[44px] flex justify-between">
       <div className="flex font-title text-[23px] lowercase italic">
-        Electric Potential
+        <Link href="/">Electric Potential</Link>
       </div>
       <div className="flex lg:ml-auto gap-10 justify-between">
         <Link href="/about-us">
