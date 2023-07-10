7b3e7a10834519551e94199e38e63fda09ddb66e
Vivian Leung <vivz753@gmail.com>
2023-07-09 22:39:01 -0700

    add description for SEO

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,8 +2,8 @@ import { Navbar } from "@/components/core"
 import "./globals.css"
 
 export const metadata = {
-  title: "Electric Potential",
-  description: "",
+  title: "Electric Potential: Gen Z-powered social change",
+  description: `Electric Potential (EP) is Gen Z's digital community and information hub for powering social change.`,
 }
 
 export default function RootLayout({
